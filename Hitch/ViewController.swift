4024453cc54a12cd3672d530f3009c9c00708f9c
Anthony Lee <ant.lee813@gmail.com>
2017-01-22 01:54:08 -0800

    Current WIP:
    ADDITIONS:
    +register button
    +register logic is work in progress
    
    CHANGES:
    *moved UI buttons around
    *changed title text
    *realigned UI features

--- Hitch/ViewController.swift
+++ Hitch/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Mark: Properties
+    
     @IBOutlet weak var txtName: UITextField!
-
     @IBOutlet weak var txtEmail: UITextField!
-    
     @IBOutlet weak var txtPhone: UITextField!
-    
     @IBAction func loginButton(_ sender: UIButton) {
     }
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        
+        if (![defaults boolForKey!@"registered"]){
+            NSLog(@"No user registerd");
+            _loginBin.hidden = YES;
+        }
+        else {
+            NSLog(@"User is registered");
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
